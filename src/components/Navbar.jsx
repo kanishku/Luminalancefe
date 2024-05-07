a8cdcede2e945d885d44320ec7bb252c70f01ad1
kanishku <k59386425@gmail.com>
2024-05-07 22:14:42 +0530

    Luminalancefe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,15 @@ import "./Navbar.css";
 
 function Navbar() {
   const [active, setActive] = useState(false);
-  const [open, setOpen] = useState(false); // State to manage menu open/close
-  const { pathname } = useLocation(); // Use useLocation hook to get the current pathname
+  const { pathname } = useLocation();
 
   const handleScroll = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -29,10 +29,6 @@ function Navbar() {
     console.log("Logout clicked");
   };
 
-  const handleUserClick = () => {
-    setOpen(!open); // Toggle the 'open' state when user clicks on user area
-  };
-
   return (
     <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
@@ -47,46 +43,42 @@ function Navbar() {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          {!currentUser.isSeller && <span>Become a seller</span>}
-          {!currentUser && <button>Join</button>}
+          {!currentUser && <span>Become a seller</span>}
           {currentUser && (
-            <div className="user" onClick={handleUserClick}>
-              {/* Display user information and options */}
+            <div className="user" onClick={handleLogout}>
               <img src="" alt="" />
               <span>{currentUser.username}</span>
-              {open && ( // Conditionally render based on the 'open' state
-                <div className="options">
-                  {currentUser.isSeller && (
-                    <>
-                      <Link className="link" to="/mygigs">
-                        Gigs
-                      </Link>
-                      <Link className="link" to="/add">
-                        Add New Gig
-                      </Link>
-                    </>
-                  )}
-                  <Link className="link" to="/orders">
-                    Orders
-                  </Link>
-                  <Link className="link" to="/messages">
-                    Messages
-                  </Link>
-                  <span className="link" onClick={handleLogout}>
-                    Logout
-                  </span>
-                </div>
-              )}
+              <div className="options">
+                {currentUser.isSeller && (
+                  <>
+                    <Link className="link" to="/mygigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <span className="link" onClick={handleLogout}>
+                  Logout
+                </span>
+              </div>
             </div>
           )}
         </div>
       </div>
+
       {/* Conditionally render menu based on active state or pathname */}
-      {active || pathname !== "/" && (
+      {(active || pathname !== "/") && (
         <>
           <hr />
           <div className="menu">
-            {/* Menu links */}
             <Link className="link menuLink" to="/">
               Graphics & Design
             </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,9 @@ function Home() {
             </p>
           </div>
           <div className="item">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/_K_-220yJJE?si=CzrUvVFfe-_AhSsc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/_K_-220yJJE?si=CzrUvVFfe-_AhSsc" frameBorder="0"></iframe>
+
+          
           </div>
         </div>
       </div>
